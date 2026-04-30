228ff394804c00851c7c3bb766919d80ed0bc68c
JunHo SON <38898759+cartoonpoet@users.noreply.github.com>
2026-05-01 00:01:19 +0900

    feat(design-system): unified export index

---
+++ apps/web/src/design-system/index.ts
@@ -0,0 +1,6 @@
+export { Button } from "./Button";
+export { Input } from "./Input";
+export { Card } from "./Card";
+export { Skeleton } from "./Skeleton";
+export { Modal } from "./Modal";
+export { vars } from "./tokens.css";
